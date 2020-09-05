b77b1025bf2d6c517750d1e75ef3c5cb3e0d7248
DongYan <279505988@qq.com>
2020-09-05 22:42:26 +0800

    微服务

---
+++ approval-api/src/main/java/com/yd/approval/api/Urls.java
@@ -0,0 +1,7 @@
+package com.yd.approval.api;
+
+/**
+ * Created by yandong on 2020/9/5.
+ */
+public class Urls {
+}
---
+++ approval-web/src/main/java/com/yd/approval/web/ApprovalWebApplication.java
@@ -0,0 +1,7 @@
+package com.yd.approval.web;
+
+/**
+ * Created by yandong on 2020/9/5.
+ */
+public class ApprovalWebApplication {
+}
